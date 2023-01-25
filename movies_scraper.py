545263a3c1ce096599bc47c11d68588d709fdd95
kiranloriya123 <110012864+kiranloriya123@users.noreply.github.com>
2023-01-25 10:22:54 +0530

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -11,7 +11,7 @@ api_key = "96a09f2f0a90da6f421518ff15b933dab0424169"
 def search_movies(query):
     movies_list = []
     movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://10moviego.blogspot.com/?m=1/?s={query.replace(' ', '+')}").text, "html.parser")
+    website = BeautifulSoup(requests.get(f"https://10moviego.blogspot.com/?s={query.replace(' ', '+')}").text, "html.parser")
     movies = website.find_all("a", {'class': 'ml-mask jt'})
     for movie in movies:
         if movie:
